fb175bdb925274ea0afc51d96b780b35770539a7
Akshay Chavan <akshaychavan.kkwedu@gmail.com>
2024-04-18 21:15:56 -0400

    1. Create user profile page 2. Create Moderator Page 3. Add Page Not Found route 4. bug fixes

---
+++ server/python utils/pyserver.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_URI
+from fastapi.middleware.cors import CORSMiddleware
+
+from tagsgen import findtags 
+
+app = FastAPI()
+
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+client = AsyncIOMotorClient(MONGO_URI)
+
+db = client.get_database("fake_so_fse")
+Tag = db.get_collection("Tag")
+# Define a Pydantic model for the data expected in the POST request
+class tagsGen(BaseModel):
+    title: str
+    description: str
+
+
+# Endpoint to handle POST requests to create a new item
+@app.post("/tag/generateTags/")
+async def generateTags(item: tagsGen):
+    text = item.title.strip() + " " + item.description.strip()
+    tags = []
+    async for tag in Tag.find({}, {"name": 1, "_id": 0}):
+        tags.append(tag['name'])
+    return findtags(text, tags)
+
+@app.get("/")
+async def root():
+    return {"message": "Python Server Works"}
+
+
+# Run this file to run the server
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("pyserver:app", host="0.0.0.0", port=8001, reload=True)
--- server/utils/textpreprocess.js
+++ server/utils/textpreprocess.js
@@ -1,6 +1,18 @@
-const preprocessing = (input) => {
-    input = input.trim();
-    return input.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#39;");
-}
+const BadWordsFilter = require("bad-words");
 
-module.exports = {preprocessing};
+const textfiltering = (input) => {
+  const filter = new BadWordsFilter();
+  return filter.isProfane(input);
+};
+
+const preprocessing = (input) => {
+  input = input.trim();
+  return input
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
+};
+
+module.exports = { preprocessing, textfiltering };
