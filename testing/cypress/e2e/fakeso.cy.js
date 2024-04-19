8984c2fe4b6dc376c72fae21915ea8cc820dc448
shawn <shawn310480@gmail.com>
2024-04-19 19:37:55 -0400

    initialize ui testing

--- testing/cypress/e2e/fakeso.cy.js
+++ testing/cypress/e2e/fakeso.cy.js
@@ -1,4 +1,20 @@
-// Template test file. Change the file to add more tests.
-describe("Add a descriptive test suite name", () => {
-  // add test cases
+
+describe("Cypress Tests repeated from React assignment", () => {
+  beforeEach(() => {
+    // Seed the database before each test
+    cy.exec("node ../server/init.js mongodb://127.0.0.1:27017/fake_so");
+
+    cy.visit("http://localhost:3000");
+    cy.get("#email").type("general")
+    cy.get("#password").type("test")
+  });
+
+  afterEach(() => {
+    // Clear the database after each test
+    cy.exec("node ../server/destroy.js mongodb://127.0.0.1:27017/fake_so");
+  });
+
+  it('1.1 | ', () => {
+    pass
+  })
 });
