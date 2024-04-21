e3ea8aae22c671c06b5853e70179918dae04ec15
Akshay Chavan <akshaychavan.kkwedu@gmail.com>
2024-04-21 00:10:27 -0400

    complete setup for docker

--- server/config.js
+++ server/config.js
@@ -8,12 +8,12 @@
 // };
 
 // uncomment for MONGO_URL for docker
-// const MONGO_URL = "mongodb://mongodb:27017/fake_so";
+const MONGO_URL = "mongodb://mongodb:27017/fake_so";
 // config for local testing, that is, without docker.
-const MONGO_URL = "mongodb://localhost:27017/fake_so";
+// const MONGO_URL = "mongodb://localhost:27017/fake_so";
 const CLIENT_URL = "http://localhost:3000";
 const port = 8000;
-const SECRET_KEY = "rhseigo23e9rgnse9s48nsweguw39g049gn&e"
+const SECRET_KEY = "rhseigo23e9rgnse9s48nsweguw39g049gn&e";
 
 module.exports = {
   MONGO_URL,
