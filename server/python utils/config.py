f1e85f72c5756437e1a5f87588450e0ee4f0d942
Akshay Chavan <akshaychavan.kkwedu@gmail.com>
2024-05-15 17:53:10 +0530

    update configs

--- server/python utils/config.py
+++ server/python utils/config.py
@@ -1,2 +1,2 @@
-# MONGO_URI = "mongodb://localhost:27017/" # enable this for local
-MONGO_URI = "mongodb://mongodb:27017/" # enable this for docker
+MONGO_URI = "mongodb://localhost:27017/" # enable this for local
+# MONGO_URI = "mongodb://mongodb:27017/" # enable this for docker
